745b9dae82c5b2c00bc55e6c7f2bf7c9b51a9594
km-saifullah <kmsaifullah16@gmail.com>
2024-11-06 11:09:47 +0600

    signup page design complete for lg screen

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -20,63 +20,116 @@ const Signup = () => {
   // handle sign up
   const handleSignUp = (e) => {
     console.log(inputFields);
+    setInputFields({
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      password: "",
+    });
     e.preventDefault();
   };
   return (
-    <main className="">
-      <section>
-        <h1>Sign Up</h1>
-        <p>Please sign up your Admin Control Panel</p>
-      </section>
-      <section>
-        <form action="">
-          <div>
-            <label htmlFor="">Full Name</label>
-            <input
-              type="text"
-              placeholder="Enter your fullname"
-              name="fullName"
-              value={inputFields?.fullName}
-              onChange={handleInput}
-            />
-          </div>
-          <div>
-            <label htmlFor="">Phone Number</label>
-            <input
-              type="text"
-              placeholder="Enter your phone number"
-              name="phoneNumber"
-              value={inputFields?.phoneNumber}
-              onChange={handleInput}
-            />
-          </div>
-          <div>
-            <label htmlFor="">Email Address</label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              name="email"
-              value={inputFields?.email}
-              onChange={handleInput}
-            />
-          </div>
-          <div>
-            <label htmlFor="">Password</label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              name="password"
-              value={inputFields?.password}
-              onChange={handleInput}
-            />
-          </div>
-          <div>
-            <Link to="/">Forgot Pasword</Link>
-          </div>
-          <div>
-            <button onClick={handleSignUp}>Log In</button>
-          </div>
-        </form>
+    <main>
+      <section className="container">
+        <section className="w-full h-[100vh] flex items-center justify-center">
+          <section className="w-[560px] h-[760px] py-12 px-7 rounded shadow-md">
+            <section className="space-y-3 pb-5">
+              <h1 className="text-4xl font-inter font-bold text-heading">
+                Sign Up
+              </h1>
+              <p className="text-text font-inter font-normal text-base">
+                Please sign up your Admin Control Panel
+              </p>
+            </section>
+            <section>
+              <form action="">
+                <div className="flex flex-col space-y-2 pb-6">
+                  <label
+                    htmlFor=""
+                    className="text-text font-inter font-normal text-base"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter your fullname"
+                    name="fullName"
+                    value={inputFields?.fullName}
+                    onChange={handleInput}
+                    className="w-full h-[60px] rounded border border-borderColor p-3 text-primary text-base font-medium font-inter outline-none placeholder:text-primary"
+                  />
+                </div>
+                <div className="flex flex-col space-y-2 pb-6">
+                  <label
+                    htmlFor=""
+                    className="text-text font-inter font-normal text-base"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter your phone number"
+                    name="phoneNumber"
+                    value={inputFields?.phoneNumber}
+                    onChange={handleInput}
+                    className="w-full h-[60px] rounded border border-borderColor p-3 text-primary text-base font-medium font-inter outline-none placeholder:text-primary"
+                  />
+                </div>
+                <div className="flex flex-col space-y-2 pb-6">
+                  <label
+                    htmlFor=""
+                    className="text-text font-inter font-normal text-base"
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    name="email"
+                    value={inputFields?.email}
+                    onChange={handleInput}
+                    className="w-full h-[60px] rounded border border-borderColor p-3 text-primary text-base font-medium font-inter outline-none placeholder:text-primary"
+                  />
+                </div>
+                <div className="flex flex-col space-y-2 pb-6">
+                  <label
+                    htmlFor=""
+                    className="text-text font-inter font-normal text-base"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    name="password"
+                    value={inputFields?.password}
+                    onChange={handleInput}
+                    className="w-full h-[60px] rounded border border-borderColor p-3  text-primary text-base font-medium font-inter outline-none placeholder:text-primary"
+                  />
+                </div>
+                <div className="flex items-center justify-end text-base text-primary font-inter font-normal">
+                  <Link to="/">Forgot Pasword?</Link>
+                </div>
+                <div className="py-3">
+                  <button
+                    onClick={handleSignUp}
+                    className="bg-secondary w-full h-[60px] text-center text-white font-inter font-normal rounded-lg cursor-pointer transition-all duration-300 ease-linear hover:bg-primary hover:text-white"
+                  >
+                    Log In
+                  </button>
+                </div>
+              </form>
+              <div>
+                <p className="text-center text-text font-inter font-normal text-base">
+                  Already have and account?{" "}
+                  <Link to="/login" className="text-primary">
+                    Login
+                  </Link>
+                </p>
+              </div>
+            </section>
+          </section>
+        </section>
       </section>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "320px",
+      md: "640px",
+      lg: "768px",
+      xl: "1140px",
+      "2xl": "1280px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "2rem",
+        lg: "4rem",
+        xl: "1rem",
+        "2xl": "2rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#181818",
+        secondary: "#DB4444",
+        heading: "#222222",
+        light: "#FAFAFA",
+        text: "rgba(0, 0, 0, 0.5)",
+        "light-green": "#00FF66",
+        "border-line": "rgba(0, 0, 0, 0.4)",
+        inputBg: "#F5F5F5",
+        borderColor: "#E7E6EC",
+      },
+      fontFamily: {
+        inter: '"Inter", sans-serif',
+      },
+    },
   },
   plugins: [],
 };
